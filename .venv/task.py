e861ebd11f7e75202f695102ecf0a7d43d812ff4
rayymaxx <rayymondodhiambo@gmail.com>
2025-06-18 22:21:11 +0300

    Modified the appearance of tasks

--- .venv/task.py
+++ .venv/task.py
@@ -2,16 +2,16 @@
 
 class Task:
     def __init__(self, id, title, description, due_date=None, completed=False):
-        self.id = id
+        self.id = id # This is used to identify the tasks in a unique way
         self.title = title
         self.description = description
         self.due_date = due_date
         self.completed = completed
 
     def mark_complete(self):
-        self.completed = True
+        self.completed = True # Marks the task as done
 
-    def to_dict(self):
+    def to_dict(self): # This converts the objet to a dictionary so it could be saved to the json file
         return {
             "id":self.id,
             "title":self.title,
@@ -20,6 +20,9 @@ class Task:
             "completed":self.completed
         }
 
-    def __str__(self):
-        status = "✅" if self.completed else "❌"
-        return f"[{status}] {self.id} - {self.title} (Due: {self.due_date or 'N/A'})"
+    def __str__(self): # This is incharge of how the task looks when printed
+        if self.completed:
+            status = "✅"
+        else:
+            status = "❌"
+        return f"\n[{status}] {self.id} - {self.title} (Due: {self.due_date or 'N/A'})"
